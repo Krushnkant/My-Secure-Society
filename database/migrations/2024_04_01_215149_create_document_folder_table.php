2d01ff92e19703225cec5b402eca8afe6ee73c7a
Vikram <ahir345@gmail.com>
2024-04-05 08:55:56 +0530

    all done change

---
+++ database/migrations/2024_04_01_215149_create_document_folder_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_folder', function (Blueprint $table) {
+            $table->bigIncrements('document_folder_id')->index();
+            $table->string('designation_name', 100);
+            $table->integer('estatus')->enum([1, 2, 3, 4])->default(1)->comment('1 - Active, 2 - InActive, 3 - Delete, 4 - Pending')->index();
+            $table->integer('created_by')->index();
+            $table->integer('updated_by')->index();
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_folder');
+    }
+};
