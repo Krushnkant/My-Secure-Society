490924868e7839245b70e72a783951767782e384
Pankaj <pankaj@listandsell.de>
2024-05-23 19:48:50 +0530

    api update

--- database/migrations/2024_02_27_040122_society_daily_post.php
+++ database/migrations/2024_02_27_040122_society_daily_post.php
@@ -39,6 +39,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('SocietyDailyPost');
+        Schema::dropIfExists('society_daily_post');
     }
 };
