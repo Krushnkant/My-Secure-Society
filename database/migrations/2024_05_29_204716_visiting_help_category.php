0173d66375cd588d41459893c0b5627c54c75ce9
Krushnkant Vadadoriya <krushnvadadoriya@gmail.com>
2024-05-29 20:52:50 +0530

    Change in Migration

---
+++ database/migrations/2024_05_29_204716_visiting_help_category.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visiting_help_category', function (Blueprint $table) {
+            $table->bigIncrements('visiting_help_category_id')->index();
+            $table->string('visiting_help_category_name', 100);
+            $table->dateTime('created_at')->nullable();
+            $table->integer('created_by')->index();
+            $table->dateTime('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->integer('updated_by')->index();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visiting_help_category');
+    }
+};
