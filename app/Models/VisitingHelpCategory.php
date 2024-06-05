0484ff14b53e9a0b51b4379544aa155c43e45ab5
Pankaj <pankaj@listandsell.de>
2024-06-05 11:07:27 +0530

    code update

--- app/Models/VisitingHelpCategory.php
+++ app/Models/VisitingHelpCategory.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class VisitingHelpCategory extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $table = 'visiting_help_category';
     protected $primaryKey = 'visiting_help_category_id';
