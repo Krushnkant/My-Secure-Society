e756440480a39d200e07419352f33c176c161649
Krushnkant Vadadoriya <krushnvadadoriya@gmail.com>
2024-06-07 10:49:00 +0530

    Staff Duty Area Migration

---
+++ database/migrations/2024_06_07_104658_staff_duty_area.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('staff_duty_area', function (Blueprint $table) {
+            $table->bigIncrements('staff_duty_area_id')->index();
+            $table->integer('society_id')->index();
+            $table->string('area_name');
+            $table->integer('estatus')->enum([1, 2, 3, 4])->default(1)->comment('1 - Active, 2 - InActive, 3 - Delete, 4 - Pending')->index();
+            $table->dateTime('created_at')->nullable();
+            $table->integer('created_by')->index();
+            $table->dateTime('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->integer('updated_by')->index();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staff_duty_area');
+    }
+};
